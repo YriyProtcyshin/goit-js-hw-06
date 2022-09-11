664ed181df311dee3f5c4a6881faaada48607382
YriyProtcyshin <yriy.protcyshin@gmail.com>
2022-09-11 13:52:06 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const navElement = document.querySelector('#ingredients');
+
+const categoryList = ingredients.map(el => {
+  let elementLi = document.createElement('li');
+  elementLi.classList.add('item');
+  elementLi.textContent = el;
+  return elementLi;
+});
+
+console.log(navElement.append(...categoryList));
