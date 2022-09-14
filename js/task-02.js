17e3d6b53b6227a17d013e6de80733e8965ed93d
YriyProtcyshin <yriy.protcyshin@gmail.com>
2022-09-14 19:51:45 +0300

    task 10

--- js/task-02.js
+++ js/task-02.js
@@ -10,3 +10,32 @@ const categoryList = ingredients.map(el => {
 });
 
 navElement.append(...categoryList);
+
+/*
+
+Задание 2
+В HTML есть пустой список ul#ingredients.
+
+<ul id="ingredients"></ul>
+
+В JavaScript есть массив строк.
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+Напиши скрипт, который для каждого элемента массива ingredients:
+
+Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+Добавит название ингредиента как его текстовое содержимое.
+Добавит элементу класс item.
+После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+
+
+*/
